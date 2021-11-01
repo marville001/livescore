df8aa88ee97893879cda82d08d1c706b949ba80f
Dan Ngetich <dancanmilgo73@gmail.com>
2021-11-01 21:37:36 +0300

    added dummy data

--- ui/src/App.js
+++ ui/src/App.js
@@ -2,23 +2,35 @@ import React from "react";
 import "./App.css";
 import LiveMatch from "./components/LiveMatch";
 import NavBar from "./components/NavBar";
-import { Card } from '@mui/material';
-import Box from '@mui/material/Box';
+import { Card } from "@mui/material";
+import Box from "@mui/material/Box";
 
 function App() {
+  const dummyData = {
+    TeamA: "NK Rudar Labin",
+    Stage: "8014",
+    TeamA_Score: "1",
+    TeamB_Logo: "https://lsm-static-prod.livescore.com/medium/enet/6038.png",
+    Country: "Croatia",
+    TeamB: "NK Istra 1961",
+    TeamA_Logo: "https://lsm-static-prod.livescore.com/medium/enet/186656.png",
+    _id: "553431",
+    TeamB_Score: "1",
+    Time: "45'",
+    Stage_Name: "Cup",
+  };
   return (
     <Card>
-      <Box border={1} borderColor="black" height={600} >
+      <Box border={1} borderColor="black" height={600}>
         <div>
           <NavBar />
-          <LiveMatch />
-          <LiveMatch />
-          <LiveMatch />
-          <LiveMatch />
+          <LiveMatch data={dummyData} />
+          <LiveMatch data={dummyData} />
+          <LiveMatch data={dummyData} />
+          <LiveMatch data={dummyData} />
         </div>
       </Box>
     </Card>
-
   );
 }
 
--- ui/src/components/LiveMatch.js
+++ ui/src/components/LiveMatch.js
@@ -1,12 +1,27 @@
 import React from "react";
 import "./LiveMatch.css";
 
-export default function LiveMatch() {
+export default function LiveMatch(data) {
   return (
     <div className="livematch__con">
-      <div className="livematch__homeTeam">Man u</div>
-      <div className="livematch__livescores">1:2</div>
-      <div className="livematch__awayTeam">Liverpool</div>
+      <div className="livematch__homeTeam">
+        <span>
+          <img src={data.TeamA_logo} alt="logo" />
+        </span>
+        {data.TeamA}
+      </div>
+      <div className="livematch__livescores">
+        <div>
+          <span>{data.TeamA_Score}</span>:<span>{data.TeamB_Score}</span>
+        </div>
+        <div>{data.Time}</div>
+      </div>
+      <div className="livematch__awayTeam">
+        {data.TeamB}
+        <span>
+          <img src={data.TeamB_logo} alt="logo" />
+        </span>
+      </div>
     </div>
   );
 }
