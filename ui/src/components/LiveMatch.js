289a159b868c358676c66854a23870cf192b65d5
Dan Ngetich <dancanmilgo73@gmail.com>
2021-11-01 14:33:41 +0300

    livematch component

---
+++ ui/src/components/LiveMatch.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./LiveMatch.css";
+
+export default function LiveMatch() {
+  return (
+    <div className="livematch__con">
+      <div className="livematch__homeTeam">Man u</div>
+      <div className="livematch__livescores">1:2</div>
+      <div className="livematch__awayTeam">Liverpool</div>
+    </div>
+  );
+}
